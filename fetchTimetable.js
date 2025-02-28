3767508e5268b48ddfefadde4c00b37bee7f2808
Yuan <lingjunyuan1044@users.noreply.github.com>
2025-02-28 15:44:50 +0800

    Amend

--- fetchTimetable.js
+++ fetchTimetable.js
@@ -3,7 +3,7 @@ config();
 
 const s3Endpoint = process.env.APU_TIMETABLE_S3;
 const intakeCode = process.env.INTAKE_CODE;
-const nonElectives = []; // Add MODID that is NOT your elective
+const nonElectives = ["CT107-3-3-TXSA"]; // Add MODID that is NOT your elective
 const grouping = "G1"; // G1 is default, grouping to filter by
 
 export async function fetchTimetable() {
